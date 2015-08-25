d09f9a41541546bc2baad8bb28a10dacbf55f2a9
valentinerutto <rutto28@gmail.com>
2015-08-25 13:27:41 +0300

    added validation to make sure the fields aint empty

--- db/migrate/20150825084714_create_jobs.rb
+++ db/migrate/20150825084714_create_jobs.rb
@@ -2,7 +2,7 @@ class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
       t.string :title
-      t.text :description
+#      t.text :description
       t.string :company
       t.string :url
 
